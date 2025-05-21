05a1a4c947ba9db4c449eb31cfdf7ac0f4e91f7d
Bondaal <160157215+Bondaal@users.noreply.github.com>
2025-05-21 17:03:42 +0300

    Update DebetCardTest.java

--- src/test/java/ru/netology/DebetCardTest.java
+++ src/test/java/ru/netology/DebetCardTest.java
@@ -14,8 +14,8 @@ public class DebetCardTest {
     public void shouldCreateNewForm() {
         open("http://localhost:9999");
         SelenideElement form = $("[data-test-id=callback-form]");
-        $("[data-test-id='name'] input").setValue("Никита Спиридонов");
-        $("[data-test-id='phone'] input").setValue("+79250561286");
+        $("[data-test-id='name'] input").setValue("Алина Бондарева");
+        $("[data-test-id='phone'] input").setValue("+79818539508");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
